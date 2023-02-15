bea5e17bcfb9ec171b972bfbf5b462d8cdb0755e
jhembe <jhembe202@gmail.com>
2023-02-15 19:29:21 +0300

    REMOVED clutter and made clean files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 window.onload = function () {
 const verseDisplay = document.getElementById("verse-display");
+const verseReference = document.getElementById("book-reference");
 const refreshButton = document.getElementById("new-verse");
 const shareButton = document.querySelector("#shareButton");
 // const newQuoteButton = document.querySelector("#new-quote");
@@ -8,24 +9,25 @@ const themeSwitch = document.querySelector(".theme-switch input");
 
 themeSwitch.addEventListener("change", switchTheme);
 
+
 async function getVerse() {
-  const apiKey = "YOUR_API_KEY_HERE";
-  const apiUrl = `https://api.biblebrain.com/v1/random?api_key=${apiKey}`;
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const verse = data.data[0].text;
-      const book = data.data[0].book;
-      const chapter = data.data[0].chapter;
-      const verseNumber = data.data[0].verse;
-      const reference = `${book} ${chapter}:${verseNumber}`;
-      verseDisplay.innerHTML = `${verse} (${reference})`;
-    })
-    .catch(error => {
-      console.error(error);
-      verseDisplay.innerHTML = "An error occurred, please try again later.";
-    });
+  const apiUrl = "https://labs.bible.org/api/?passage=random&type=json";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const verse = data[0].text;
+    const book = data[0].bookname;
+    const chapter = data[0].chapter;
+    const verseNumber = data[0].verse;
+    const reference = `${book} ${chapter}:${verseNumber}`;
+    verseDisplay.innerHTML = `${verse} (${reference})`;
+    verseReference.innerHTML = "~ " + `${reference}`;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("verse-display").innerHTML = "An error occurred, please try again later.";
+  }
 }
+
 
 function switchTheme(){
   document.body.classList.toggle("dark")
@@ -37,7 +39,7 @@ function shareQuote() {
   // const twitterUrl = `https://twitter.com/intent/tweet?text=${encodedQuote}`;
   // window.open(twitterUrl, "_blank");
 
-const quoteToShare = quote.innerText + " " + author.innerText;
+const quoteToShare = verseDisplay.innerText + " " + verseReference.innerText;
 const encodedQuote = encodeURIComponent(quoteToShare);
 const whatsappUrl = `https://wa.me/?text=${encodedQuote}`;
 window.open(whatsappUrl, "_blank");
@@ -53,108 +55,3 @@ shareButton.addEventListener("click",shareQuote)
 }
 
 
-
-// import axios from "axios";
-
-// const quoteContainer = document.querySelector(".quote-container");
-// const quote = document.querySelector(".quote");
-// const author = document.querySelector(".author");
-// const newQuoteBtn = document.querySelector("#new-quote");
-// const shareBtn = document.querySelector("#shareButton");
-// const themeSwitch = document.querySelector(".theme-switch input");
-
-// async function getQuote() {
-//   try {
-//     const response = await axios.get("https://api.quotable.io/random");
-//     quote.innerText = response.data.content;
-//     author.innerText = `- ${response.data.author}`;
-//   } catch (error) {
-//     console.log("Whoops, no quote", error);
-//   }
-// }
-
-// function switchTheme() {
-//     document.body.classList.toggle("dark");
-//   }
-
-// function shareQuote() {
-//   const quoteToShare = quote.innerText + " " + author.innerText;
-//   const encodedQuote = encodeURIComponent(quoteToShare);
-//   const twitterUrl = `https://twitter.com/intent/tweet?text=${encodedQuote}`;
-//   window.open(twitterUrl, "_blank");
-// }
-
-//   getQuote();
-//   refreshButton.addEventListener("click", getQuote);
-//   shareButton.addEventListener("click", shareQuote);
-
-
-// window.onload = function () {
-//   const quoteContainer = document.querySelector(".quote-container");
-//   const quote = document.querySelector(".quote");
-//   const author = document.querySelector(".author");
-// const refreshButton = document.querySelector("#new-quote");
-// const shareButton = document.querySelector("#shareButton");
-//   // const newQuoteButton = document.querySelector("#new-quote");
-//   const themeSwitch = document.querySelector(".theme-switch input");
-
-//   // newQuoteButton.addEventListener("click", getQuote);
-//   themeSwitch.addEventListener("change", switchTheme);
-
-//   async function getQuote() {
-//     axios
-//       // .get("https://api.polygon.io/v3/quotes/AAPL?apiKey=nAZ2wdrjT3oPzz0LI4iHAMVqCYTetMps")
-//       // .get("https://quotes.rest/qod.json?category=inspire")
-//       .get("https://api.quotable.io/random")
-//       .then(function (response) {
-//         // quote.innerHTML = "Hello there, this is the quote section";
-//         quote.innerHTML = response.data.content;
-//         // quote.innerHTML = response.data.contents.quotes.quote;
-//         // author.innerHTML = "-" + "Joseph Gabriel";
-//         author.innerHTML = "~ " + response.data.author;
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//       });
-//   }
-
-//   function switchTheme() {
-//     document.body.classList.toggle("dark");
-//   }
-
-//   // function shareQuote() {
-//   //   if (navigator.share) {
-//   //     navigator.share({
-//   //       title: "Quote of the Day",
-//   //       text: quoteDisplay.innerHTML,
-//   //     })
-//   //       .then(() => console.log("Quote shared successfully"))
-//   //       .catch(error => console.error("Error sharing quote", error));
-//   //   } else {
-//   //     alert("Your device does not support sharing quotes. Please copy the text manually.");
-//   //   }
-//   // }
-
-
-
-// function shareQuote() {
-//   // const quoteToShare = quote.innerText + " " + author.innerText;
-//   // const encodedQuote = encodeURIComponent(quoteToShare);
-//   // const twitterUrl = `https://twitter.com/intent/tweet?text=${encodedQuote}`;
-//   // window.open(twitterUrl, "_blank");
-
-// const quoteToShare = quote.innerText + " " + author.innerText;
-// const encodedQuote = encodeURIComponent(quoteToShare);
-// const whatsappUrl = `https://wa.me/?text=${encodedQuote}`;
-// window.open(whatsappUrl, "_blank");
-
-// }
-
-
-
-
-
-//   getQuote();
-//   refreshButton.addEventListener("click", getQuote);
-//   shareButton.addEventListener("click", shareQuote);
-// }
